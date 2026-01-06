7c9e2704bb3479b3f8ac62dbb812a1806e2f1f36
arvie993 <108028984+arvie99@users.noreply.github.com>
2026-01-06 15:25:44 -0500

    Add PostgreSQL support for Render deployment

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def admin_only(f):
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///posts.db')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
